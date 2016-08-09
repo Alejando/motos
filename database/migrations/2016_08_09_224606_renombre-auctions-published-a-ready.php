3c805e113f4b7ea190e9863da09314ff3c116ccf
Francisco Diaz <jdiaz@estrasol.com.mx>
2016-08-09 17:53:29 -0500

    Se cambia la columna published a ready en la tabla auctions

---
+++ database/migrations/2016_08_09_224606_renombre-auctions-published-a-ready.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenombreAuctionsPublishedAReady extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::table('auctions',function(Blueprint $table){
+            $table->renameColumn('published', 'ready');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('auctions', function(Blueprint $table){
+            $table->renameColumn('ready','published');
+        });
+    }
+}
