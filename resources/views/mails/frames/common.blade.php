870de3334a77924c8d7cc385fe8c935f35aff0d2
Jared Pardo <lpardo@estrasol.com.mx>
2016-08-15 14:45:49 -0500

    Adaptación de base correo y variables para mail sender

--- resources/views/mails/frames/common.blade.php
+++ resources/views/mails/frames/common.blade.php
@@ -1,21 +1,26 @@
 <html>
     <head>
-        <style>
-            .divSup {
-                border: 1px solid black;padding: 50px;
-                color:red;
-                font-size:50px;
-            }
-        </style>
+        <meta charset="UTF-8">
         @yield('style')
     </head>
     <body>
-        <div class="divSup">
-            Frame Común
-            <div style="border: 1px solid black;padding: 50px;">
-                @yield('message')
+        <div class="contenedor" style="min-width: 300px; max-width: 600px; border: 1px solid #284f53;margin: auto; text-align: center;font-family: Tahoma, Geneva, sans-serif;">
+            <div class="cabeza" style="background-image: url('{{asset('img/mail/bg-head-mail.jpg')}}');background-repeat: no-repeat;">
+		<img src="{{$message->embed('img/mail/logo-glimglam.png')}}" alt="Logo Glim Glam" style="margin: 17px auto;">
+            </div>
+            
+            @yield('message')
+            
+            <div class="pie" style="background-color: #343233;padding: 5px 0;height: 40px;">
+                    <div style="text-align: left;display: inline-block;float: left;margin-left: 10px;margin-top: 3px;">
+                            <a href="https://www.facebook.com/GlimGlam.mx/"><img src="{{asset('img/mail/ico-facebook.png')}}"></a>
+                            <a href="https://www.instagram.com/glimglammx/"><img src="{{asset('img/mail/ico-instagram.png')}}"></a>
+                            <a href="https://www.youtube.com/GlimGlam.mx/"><img src="{{asset('img/mail/ico-youtube.png')}}"></a>
+                    </div>
+                    <div style="text-align: right;display: inline-block;float: right;margin-right: 10px;">
+                            <img src="{{asset('img/mail/logo-glimglam-footer.png')}}">
+                    </div>
             </div>
         </div>
-
     </body>
 </html>   
--- resources/views/mails/html/welcome.blade.php
+++ resources/views/mails/html/welcome.blade.php
@@ -9,9 +9,50 @@
 @stop
 
 @section('message')
+<div class="banner" style="background-image: url('{{asset('img/mail/bg-banner-mail.jpg')}}');padding: 18px 0;color: #fff;">
+        <h2 style="text-transform: uppercase;margin: 40px auto;font-weight: 100;font-size: 40px;">¡BIENVENIDA <span style="">Karla!</span></h2>
+</div>
+<div class="mensaje" style="color: #003937; padding: 0 10%;">
+        <p style="margin: 16px auto;font-size: 20px;margin-top: 30px;">Gracias por registrarse en <a href="http://glimglam.mx" style="color: #003937;">glimglam.mx</a></p>
+        <p style="margin: 16px auto;">A partir de este momento, podrás acceder a las subastas de nuestros productos exclusivos, asimismo recibirás notificaciones de subastas por comenzar.</p>
+        <hr>
+        <a href="http://glimglam.mx/login" style="color: #fff; text-decoration: none;">
+            <div style="margin: 10px auto 30px auto;border: 1px solid #3c9ba2;padding: 10px; width: 100px;background-color: #00bcb6;display: inline-block;">Iniciar Sesión</div>
+        </a>
+</div>
+<div class="datos" style="background-color: #efefef; color: #003937;padding: 15px 30px;">
+        <div style="width:49%;text-align: left;display: inline-block;vertical-align: top;">
+                <h3 style="text-align: center;">Tus datos</h3>
+                <p style="text-align: center;">Karla Pérez</p>
+                <p style="text-align: center;">kperez@gmail.com</p>
+                <p style="text-align: center;">Intereses:</p>
+                <table style="text-align: center; font-size: 12px;" width="100%">
+                    <tr>
+                      <td style="padding: 7px 0;">Joyería</td>
+                      <td style="padding: 7px 0;">Moda/Accesorios</td>
+                    </tr>
+                    <tr>
+                      <td style="padding: 7px 0;">Electrónica</td>
+                      <td style="padding: 7px 0;"></td>
+                    </tr>
+                    <tr>
+                      <td style="padding: 7px 0;">Zapatos</td>
+                      <td style="padding: 7px 0;"></td>
+                    </tr>
+                  </table>
+        </div>
+        <div style="width:49%;display: inline-block;vertical-align: top;">
+                <h3 style="text-align: center;">Acerca de GlimGlam</h3>
+                <img src="{{asset('img/mail/btn-videoplayoverlay.png')}}" style="width: 90%;">
+        </div>
+</div>
+<p style="font-size: 14px;"><span style="font-size: 22px; font-weight: bold;">Gracias,</span><br>Equipo GlimGlam</p>
+@stop
 
+
+@section('oculto')
 Welcome! <span class="welcome">s{{$user->name}}</span>
     {{$user->email}}
     {{env('EMAIL_TEST_DEVELOPER')}}
     <img src="{{$message->embed("img/logo.png")}}">
-@stop   
+@stop
