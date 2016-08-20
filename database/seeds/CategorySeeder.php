0d55781ee8636f322c3b7184df624707209801cf
Francisco Diaz <jdiaz@estrasol.com.mx>
2016-08-20 13:44:31 -0500

    se actualiza el seeder de categoria, para crear las categorias por defecto

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -10,15 +10,40 @@ class CategorySeeder extends Seeder {
      * @return void
      */
     public function run() {
-        
-        factory(\GlimGlam\Models\Category::class, 5)
-            ->create()
-            ->each(function($category){
-                factory(\GlimGlam\Models\Category::class, rand(3, 5))
-                    ->create([
-                    'parentCategory' => $category->id
+        $items = [
+            ['Bolsa', []],
+            ['Cinturones', []],
+            ['Joyería', ['Aretes/Pulseras']],
+            ['Bolsa', []],
+            ['Tarjeteros', []],
+            ['Billeteras', []],
+            ['Lentes de sol', []],
+            ['Textiles', ['Corbatas', 'Corbata/Pañuelos/Manuernas']]
+        ];
+        foreach ($items as $item) {
+            $parent = \GlimGlam\Models\Category::create([
+                        'name' => $item[0]
+            ]);
+            foreach ($item[1] as $i) {
+                \GlimGlam\Models\Category::create([
+                    'name' => $i,
+                    'parentCategory' => $parent->id
                 ]);
-            });
+            }
+        }
+
+
+
+
+
+//        factory(\GlimGlam\Models\Category::class, 5)
+//            ->create()
+//            ->each(function($category){
+//                factory(\GlimGlam\Models\Category::class, rand(3, 5))
+//                    ->create([
+//                    'parentCategory' => $category->id
+//                ]);
+//            });
     }
 
 }
