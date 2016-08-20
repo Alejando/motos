0e29c3f8bf02e562e4820eba764f4aad90cc234f
Francisco Diaz <jdiaz@estrasol.com.mx>
2016-08-20 13:27:53 -0500

    Se agrega el metodo getByName

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,8 +1,11 @@
 <?php
 namespace GlimGlam\Models;
-
+/**
+ * @property string $name Nombre de la categoria
+ * @property int|null $parentCategory Categoria padre
+ */
 class Category extends \GlimGlam\Libs\CoreUtils\ModelBase{
-//    public $timestamps = true;
+    
     public function parentCategory() {
         return $this->belongsTo(Category::class, 'parentCategory');
     }
@@ -10,7 +13,12 @@ class Category extends \GlimGlam\Libs\CoreUtils\ModelBase{
     public function subCategories(){
        return $this->hasMany(Category::class, 'parentCategory');
     }
+    
     public static function getRandomParentCategory() {
        return self::whereNull('parentCategory')->orderBy(\DB::raw('RAND()'))->limit(1)->get()->get(0);
+    }
+    
+    public static function getByName ($name) {
+        return self::where('name','=', $name)->get(0)->get(0);
     }
 }
