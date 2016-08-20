9bba26d337e59e6ae1a4be756762428752e6dc3e
Omar Hernandez <ohernandez@estrasol.com.mx>
2016-08-20 13:55:01 -0500

    Modificación de seeder para tabla de subastas, productos dummy

--- database/seeds/AuctionsSeeder.php
+++ database/seeds/AuctionsSeeder.php
@@ -9,19 +9,336 @@ class AuctionsSeeder extends Seeder
      *
      * @return void
      */
+    
+    //Genero: 1 hombre, 0 mujer
     public function run() {
+        /*
+         * //Definición de time zone y UTC
+         * $tzMx = new DateTimeZone('America/Mexico_City');
+         * $utc = new DateTimeZone('UTC');
+         * $date = new DateTime('2016-07-19 15:01:19', $tzMx);
+         * //echo $date->format('Y-m-d H:i:s');
+         * //echo "\n";
+         * $date->setTimezone($utc);
+         * //echo $date->format('Y-m-d H:i:s');
+         * (new DateTime('2016-07-19T15:01:19+0000'))->format('Y-m-d H:i:s');
+         * $dates = [];
+         */
+        $items = [
+            [//1
+                'category'=>  GlimGlam\Models\Category::getByName('Bolsa')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB001',
+                'barcode'=>'',
+                'title'=>'Coach - Bolsa TEA coleción 1941',
+                'real_price'=>14000.01,
+                'cover'=>646.00,
+                'min_offer'=>5.08,
+                'max_offer'=>5.70,
+                'bids'=>18,
+                'max_price'=>2975.24,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Bolsa artesanal de piel con adorno de flores en cuero colocados a mano sobre una pequeña silueta. 
+    Correa de cadena tejida a través de una cinta delgada de cuero. Monedero Francés en el interior.
+    Color: Negro/Latón antiguo
+    Forro: Cuero
+    Medidas: 22.86x14x5.70 cm.
+    Correa : 56 cm.',
+                'start_date'=>'2016-07-19 15:01:19',
+                'end_date'=>'2016-08-12 04:00:04',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//2
+                'category'=>GlimGlam\Models\Category::getByName('Cinturones')->id,
+                'sub_category'=>null,
+                'target'=>1,
+                'code'=>'SUB002',
+                'barcode'=>'679311',
+                'title'=>'Ferragamo - Cinturón con hebilla de doble Gancini',
+                'real_price'=>5399.80,
+                'cover'=>249.00,
+                'min_offer'=>1.96,
+                'max_offer'=>2.20,
+                'bids'=>18,
+                'max_price'=>1147.55,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Cinturón en piel de becerro con grano, reversible, con una hebilla de doble Gancini en gris plomo. 
+Es un cinturón ajustable y se puede cortar a medida para un ajuste personalizado.
+Hecho en Italia',
+                'start_date'=>'2016-09-19 00:00:00',
+                'end_date'=>'2016-10-12 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//3
+                'category'=>GlimGlam\Models\Category::getByName('Joyería')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB003',
+                'barcode'=>'',
+                'title'=>'Ferragamo - Juego de aretes y pulsera de Gancino',
+                'real_price'=>5445.04,
+                'cover'=>251.00,
+                'min_offer'=>1.98,
+                'max_offer'=>2.22,
+                'bids'=>18,
+                'max_price'=>1157.17,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Aretes de Gancino en Latón y Pulsera reversible con Gancino en color Negro/Camel.
+Hecho en Italia',
+                'start_date'=>'2016-10-01 00:00:00',
+                'end_date'=>'2016-10-10 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//2 - 1
+                'category'=>GlimGlam\Models\Category::getByName('Bolsa')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB004',
+                'barcode'=>'',
+                'title'=>'Coach - Bolsa TEA coleción 1941',
+                'real_price'=>14000.01,
+                'cover'=>646.00,
+                'min_offer'=>5.08,
+                'max_offer'=>5.70,
+                'bids'=>18,
+                'max_price'=>2975.24,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Bolsa artesanal de piel con adorno de flores en cuero colocados a mano sobre una pequeña silueta. 
+    Correa de cadena tejida a través de una cinta delgada de cuero. Monedero Francés en el interior.
+    Color: Negro/Latón antiguo
+    Forro: Cuero
+    Medidas: 22.86x14x5.70 cm.
+    Correa : 56 cm.',
+                'start_date'=>'2016-09-20 15:01:19',
+                'end_date'=>'2016-09-25 04:00:04',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//2 - 2
+                'category'=>GlimGlam\Models\Category::getByName('Cinturones')->id,
+                'sub_category'=>null,
+                'target'=>1,
+                'code'=>'SUB005',
+                'barcode'=>'679311',
+                'title'=>'Ferragamo - Cinturón con hebilla de doble Gancini',
+                'real_price'=>5399.80,
+                'cover'=>249.00,
+                'min_offer'=>1.96,
+                'max_offer'=>2.20,
+                'bids'=>18,
+                'max_price'=>1147.55,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Cinturón en piel de becerro con grano, reversible, con una hebilla de doble Gancini en gris plomo. 
+Es un cinturón ajustable y se puede cortar a medida para un ajuste personalizado.
+Hecho en Italia',
+                'start_date'=>'2016-10-19 00:00:00',
+                'end_date'=>'2016-10-25 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//2 - 3
+                'category'=>GlimGlam\Models\Category::getByName('Joyería')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB006',
+                'barcode'=>'',
+                'title'=>'Ferragamo - Juego de aretes y pulsera de Gancino',
+                'real_price'=>5445.04,
+                'cover'=>251.00,
+                'min_offer'=>1.98,
+                'max_offer'=>2.22,
+                'bids'=>18,
+                'max_price'=>1157.17,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Aretes de Gancino en Latón y Pulsera reversible con Gancino en color Negro/Camel.
+Hecho en Italia',
+                'start_date'=>'2016-11-01 00:00:00',
+                'end_date'=>'2016-11-11 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//3 - 1
+                'category'=>GlimGlam\Models\Category::getByName('Bolsa')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB007',
+                'barcode'=>'',
+                'title'=>'Coach - Bolsa TEA coleción 1941',
+                'real_price'=>14000.01,
+                'cover'=>646.00,
+                'min_offer'=>5.08,
+                'max_offer'=>5.70,
+                'bids'=>18,
+                'max_price'=>2975.24,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Bolsa artesanal de piel con adorno de flores en cuero colocados a mano sobre una pequeña silueta. 
+    Correa de cadena tejida a través de una cinta delgada de cuero. Monedero Francés en el interior.
+    Color: Negro/Latón antiguo
+    Forro: Cuero
+    Medidas: 22.86x14x5.70 cm.
+    Correa : 56 cm.',
+                'start_date'=>'2016-08-25 15:01:19',
+                'end_date'=>'2016-08-30 04:00:04',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//3 - 2
+                'category'=>GlimGlam\Models\Category::getByName('Cinturones')->id,
+                'sub_category'=>null,
+                'target'=>1,
+                'code'=>'SUB008',
+                'barcode'=>'679311',
+                'title'=>'Ferragamo - Cinturón con hebilla de doble Gancini',
+                'real_price'=>5399.80,
+                'cover'=>249.00,
+                'min_offer'=>1.96,
+                'max_offer'=>2.20,
+                'bids'=>18,
+                'max_price'=>1147.55,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Cinturón en piel de becerro con grano, reversible, con una hebilla de doble Gancini en gris plomo. 
+Es un cinturón ajustable y se puede cortar a medida para un ajuste personalizado.
+Hecho en Italia',
+                'start_date'=>'2016-09-03 00:00:00',
+                'end_date'=>'2016-09-11 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//3 - 3
+                'category'=>GlimGlam\Models\Category::getByName('Joyería')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB009',
+                'barcode'=>'',
+                'title'=>'Ferragamo - Juego de aretes y pulsera de Gancino',
+                'real_price'=>5445.04,
+                'cover'=>251.00,
+                'min_offer'=>1.98,
+                'max_offer'=>2.22,
+                'bids'=>18,
+                'max_price'=>1157.17,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Aretes de Gancino en Latón y Pulsera reversible con Gancino en color Negro/Camel.
+Hecho en Italia',
+                'start_date'=>'2016-10-04 00:00:00',
+                'end_date'=>'2016-10-15 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//4 - 1
+                'category'=>GlimGlam\Models\Category::getByName('Bolsa')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB010',
+                'barcode'=>'',
+                'title'=>'Coach - Bolsa TEA coleción 1941',
+                'real_price'=>14000.01,
+                'cover'=>646.00,
+                'min_offer'=>5.08,
+                'max_offer'=>5.70,
+                'bids'=>18,
+                'max_price'=>2975.24,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Bolsa artesanal de piel con adorno de flores en cuero colocados a mano sobre una pequeña silueta. 
+    Correa de cadena tejida a través de una cinta delgada de cuero. Monedero Francés en el interior.
+    Color: Negro/Latón antiguo
+    Forro: Cuero
+    Medidas: 22.86x14x5.70 cm.
+    Correa : 56 cm.',
+                'start_date'=>'2016-09-24 15:01:19',
+                'end_date'=>'2016-09-25 04:00:04',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//4 - 2
+                'category'=>GlimGlam\Models\Category::getByName('Cinturones')->id,
+                'sub_category'=>null,
+                'target'=>1,
+                'code'=>'SUB011',
+                'barcode'=>'679311',
+                'title'=>'Ferragamo - Cinturón con hebilla de doble Gancini',
+                'real_price'=>5399.80,
+                'cover'=>249.00,
+                'min_offer'=>1.96,
+                'max_offer'=>2.20,
+                'bids'=>18,
+                'max_price'=>1147.55,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Cinturón en piel de becerro con grano, reversible, con una hebilla de doble Gancini en gris plomo. 
+Es un cinturón ajustable y se puede cortar a medida para un ajuste personalizado.
+Hecho en Italia',
+                'start_date'=>'2016-10-17 00:00:00',
+                'end_date'=>'2016-10-19 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ],[//4 - 3
+                'category'=>GlimGlam\Models\Category::getByName('Joyería')->id,
+                'sub_category'=>null,
+                'target'=>0,
+                'code'=>'SUB012',
+                'barcode'=>'',
+                'title'=>'Ferragamo - Juego de aretes y pulsera de Gancino',
+                'real_price'=>5445.04,
+                'cover'=>251.00,
+                'min_offer'=>1.98,
+                'max_offer'=>2.22,
+                'bids'=>18,
+                'max_price'=>1157.17,
+                'user_quota'=>30,
+                'users_limit'=>100,
+                'delay'=>15,
+                'max_user_quiet'=>2,
+                'death_time'=>0,
+                'description'=>'Aretes de Gancino en Latón y Pulsera reversible con Gancino en color Negro/Camel.
+Hecho en Italia',
+                'start_date'=>'2016-11-11 00:00:00',
+                'end_date'=>'2016-11-13 00:00:00',
+                'ready'=>1,
+                'status'=>  GlimGlam\Models\Auction::STATUS_STAND_BY
+            ]
+        ];
         
-//        $faker= Faker\Factory::create();
-//        $starDate = $faker->dateTimeBetween('-1 months', '+2 months');
-//       echo $starDate->format("Y-m-d H:i:s");
-//       $starDate->add(new DateInterval('P1DT1H'));
-////       echo "\n".$starDate->format("Y-m-d H:i:s");
-//       die();
-//        echo sprintf("%0d", rand(1, 10));
-//       die();
-        factory(\GlimGlam\Models\Auction::class, 200)
-            ->create();
-           
+        foreach($items as $item){
+            \GlimGlam\Models\Auction::create($item);
+        }
     }
 
 }
