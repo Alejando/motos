b699a5edfd138c2025d6bc210c8e57cfad6afc49
Francisco Diaz <jdiaz@estrasol.com.mx>
2016-08-21 19:49:10 -0500

    validación de sesion inicial en EnrollmentController::userIsEnrollment

--- app/Http/Controllers/Api/EnrollmentController.php
+++ app/Http/Controllers/Api/EnrollmentController.php
@@ -4,10 +4,13 @@ class EnrollmentController extends \GlimGlam\Libs\CoreUtils\ApiRestController{
     protected static $model = \GlimGlam\Models\Enrollment::class;
     
     public function userIsEnrollment($auctionCode){
-        $user = \Auth::user();
-        return [
-            'enrollment' => \GlimGlam\Models\Enrollment::userIsEnrollment($auctionCode, $user->id)
-        ];
+        if(\Auth::check()){
+            $user = \Auth::user();
+            return [
+               'enrollment' => \GlimGlam\Models\Enrollment::userIsEnrollment($auctionCode, $user->id)
+            ];
+        }
+        return false;
     }
     
 }
