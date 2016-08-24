3d8b018c84fdbde774d46012f1d569cd6d486493
Omar Hernandez <ohernandez@estrasol.com.mx>
2016-08-24 10:48:27 -0500

    Secciones informativas hardcode

--- resources/views/public/pages/base-contents.blade.php
+++ resources/views/public/pages/base-contents.blade.php
@@ -1,6 +1,6 @@
 @extends('public.base')
 @section('body')
-<section class="container genieric-content">
+<section class="container generic-content">
     <h2 class="content-title">@yield('content-title')</h2>
     <article class="content-body">
         @yield('body-title')
