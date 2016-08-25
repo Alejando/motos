626783ffe6a701dfaf7bf240f697e9dbfe1d803a
Omar Hernandez <ohernandez@estrasol.com.mx>
2016-08-25 10:21:52 -0500

    Secciones informativas hardcode

---
+++ resources/views/public/menus/nav-search.blade.php
@@ -0,0 +1,29 @@
+<nav class="menu-principal transition-0-5">
+    <p class="label-menu label-gold first-label">Filtrar por:</p>
+    <div class="cerrar-menu"></div>
+    <div class="menu-solid-container">
+        <div class="menu-filtros">
+            <input type="text" placeholder="Ej. Sony, Apple, Samsung" class="input-filtros" />
+            <p class="label-menu">Marca</p>
+        </div>
+        <div class="menu-filtros">
+            <input type="text" placeholder="Ej. ¿?" class="input-filtros" />
+            <p class="label-menu">Tipo</p>
+        </div>
+        <div class="menu-filtros">
+            <input type="text" placeholder="Ej. ¿?" class="input-filtros" />
+            <p class="label-menu">Sub tipo</p>
+        </div>
+        <div class="menu-filtros">
+            <p class="label-menu label-gold">Rango de cover</p>
+            <div class="min-max-container">
+                <input type="text" placeholder="$10000.00" id="input-min" class="input-filtros input-rango input-min"/>
+                <label class="label-menu">Mínimo</label>
+            </div>
+            <div class="min-max-container">
+                <input type="text" placeholder="$10000.00" id="input-max" class="input-filtros input-rango input-max"/>
+                <label class="label-menu">Máximo</label>
+            </div>
+        </div>
+    </div>
+</nav>
--- resources/views/public/menus/nav-top.blade.php
+++ resources/views/public/menus/nav-top.blade.php
@@ -9,6 +9,8 @@
 </div>
 <a href="{{Auth::check() ? route('my-profile') : url('/login') }}" class="item-header text-center link-perfil" id="link-perfil" data-toggle="tooltip" title="Mi Perfil">
     <i class="fa fa-user" aria-hidden="true"></i>
-</a><div class="item-header transition-0-5 text-center link-menu" id="link-menu" data-toggle="tooltip" title="Filtrar Subasta">
+</a><div class="item-header transition-0-5 text-center link-menu" id="link-search" data-toggle="tooltip" title="Buscar subasta">
+    <i class="fa fa-search" aria-hidden="true"></i>
+</div><div class="item-header transition-0-5 text-center link-menu" id="link-menu" data-toggle="tooltip" title="Menu">
     <i class="fa fa-sliders" aria-hidden="true"></i>
-</div> 
+</div>
