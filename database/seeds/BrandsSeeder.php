b2880acbaa70d6abcd0eb05d4bc1c15e95bbd561
Omar Hernandez <ohernandez@estrasol.com.mx>
2016-09-28 10:27:27 -0500

    Se ajustaron los seeders con datos ficticios

--- database/seeds/BrandsSeeder.php
+++ database/seeds/BrandsSeeder.php
@@ -10,7 +10,25 @@ class BrandsSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        factory(DwSetpoint\Models\Brand::class,10)->create();
+        //factory(DwSetpoint\Models\Brand::class,10)->create();
+        $items = [
+            [//1
+                'name'=>'Nike'
+            ],[//2
+                'name'=>'Adidas'
+            ],[//3
+                'name'=>'Wilson'
+            ],[//4
+                'name'=>'Babolat'
+            ],[//5
+                'name'=>'Prince'
+            ],[//6
+                'name'=>'Yonex'
+            ]
+        ];
+        foreach($items as $item){
+            DwSetpoint\Models\Brand::create($item);
+        }
     }
 
 }
