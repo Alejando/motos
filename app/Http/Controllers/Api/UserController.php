425d58525f90ad3812c8779734421387e4d3029b
Francisco Diaz <jdiaz@estrasol.com.mx>
2016-11-23 10:44:30 -0600

    Actualización del método storage para la creación de usuarios (Clientes)

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,18 @@ use DwSetpoint\Models\User;
 use Illuminate\Support\Facades\Input;
 class UserController extends \DevTics\LaravelHelpers\Rest\ApiRestController {
     protected static $model = \DwSetpoint\Models\User::class;
+    public function store(\Illuminate\Http\Request $request) {
+        $user = \Auth::user();
+        if ($user) {
+            return parent::store($request);
+        } else {
+            $r =  parent::store($request);
+            $r['model']->profile_id = \DwSetpoint\Models\Profile::TYPE_CLIENT;
+            $r['model']->save();            
+            return $r;
+        }
+    }
+    
     
     public function addBookmark() {
         $id_product = Input::get('id_product');
