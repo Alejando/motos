75542f91201076a220f371b356d587214d88eee0
Francisco Diaz <jdiaz@estrasol.com.mx>
2017-01-03 15:33:52 -0600

    Correcciones en seeders

--- fw/database/seeds/DatabaseSeeder.php
+++ fw/database/seeds/DatabaseSeeder.php
@@ -10,20 +10,19 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        // $this->call(BrandsSeeder::class);
-        // $this->call(ColorsSeeder::class);
-        // $this->call(SizesSeeder::class);
-        // $this->call(ProfilesSeeder::class);
+        $this->call(BrandsSeeder::class);
+        $this->call(ColorsSeeder::class);
+        $this->call(SizesSeeder::class);
+        $this->call(ProfilesSeeder::class);
         $this->call(UsersSeeder::class);
-        // $this->call(ProductsSeeder::class);
-        // $this->call(ColorProductSeeder::class);
-        // $this->call(StocksSeeder::class);
-        // $this->call(CategoriesSeeder::class);
-        // $this->call(AddressesSeeder::class);
-        // $this->call(PostalCodeGroupsSeeder::class);
-        // $this->call(PostalCodesSeeder::class);
-        // $this->call(CategoriesProductsSeeder::class);
-        
+        $this->call(StocksSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(AddressesSeeder::class);
+        $this->call(PostalCodeGroupsSeeder::class);
+        $this->call(PostalCodesSeeder::class);
+        $this->call(CategoriesProductsSeeder::class);
+        $this->call(ProductsSeeder::class);
+        $this->call(ColorProductSeeder::class);
     }
 
 }
