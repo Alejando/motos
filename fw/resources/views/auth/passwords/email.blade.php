88febc5563209860e181adaf204e2352e905bb3a
gmanzanares <gmanzanares@192.168.2.95>
2017-01-05 18:51:49 -0600

    Funcionamiento y Vistas de restablecer contraseña

--- fw/resources/views/auth/passwords/email.blade.php
+++ fw/resources/views/auth/passwords/email.blade.php
@@ -1,12 +1,13 @@
-@extends('layouts.app')
-
-<!-- Main Content -->
-@section('content')
+@extends('public.base',[
+    'showOffert'=>false,
+    'showBannerBottom' => false
+])
+@section('body')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+        <div class="col-md-8 col-md-offset-1">
+            <div class="panel panel-default" style="margin: 100px auto;">
+                <div class="panel-heading">Restablecer la contraseña</div>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
@@ -18,7 +19,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">Dirección de correo</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
@@ -34,7 +35,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
+                                    <i class="fa fa-btn fa-envelope"></i> Enviar enlace de restablecer contraseña
                                 </button>
                             </div>
                         </div>
@@ -44,4 +45,4 @@
         </div>
     </div>
 </div>
-@endsection
+@stop
