6c2fe3cd14a3826d434314541e867923c006a504
gmanzanares <gmanzanares@192.168.2.95>
2017-01-25 15:11:27 -0600

    add iframe pdf

--- fw/resources/views/public/pages/cart/success-conekta-oxxo.blade.php
+++ fw/resources/views/public/pages/cart/success-conekta-oxxo.blade.php
@@ -11,6 +11,7 @@
     <div class="col-md-10 col-md-offset-1 margentop20">
         <p class="subtitulo">* Puedes imprimir o descargar tu formato para realizar el pago en el OXXO</p>
         <div class="botonera margentop50 text-center">
+        	<iframe style="display:none;" class="iframe-print" src="{{route('cart.conecta-oxxo-format', ['format' => 'html', 'order'=> $order->id ])}}"></iframe>
 	    	<a href="" ng-click="print()">Imprimir</a>
 	    	<a href="" ng-click="download()">Descargar</a>
 	    </div>
