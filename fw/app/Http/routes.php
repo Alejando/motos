9d7fab4113837c09bc42a8172aee9f9dd23f1664
gmanzanares <gmanzanares@192.168.2.95>
2017-02-27 18:23:41 -0600

    Maquetacion de contacto

--- fw/app/Http/routes.php
+++ fw/app/Http/routes.php
@@ -14,13 +14,17 @@ Route::get('acerca-de', function () {
 });
 
 Route::get('filosofia', function () {
-    return view('public/pages/notice-privacy');
+    return view('public/pages/about-ktm');
 });
 
 Route::get('vision', function () {
-    return view('public/pages/notice-privacy');
+    return view('public/pages/about-ktm');
 });
 
 Route::get('compromiso', function () {
-    return view('public/pages/notice-privacy');
+    return view('public/pages/about-ktm');
+});
+
+Route::get('contacto', function () {
+    return view('public/pages/contact');
 });
--- fw/resources/views/public/blocks/main-title.blade.php
+++ fw/resources/views/public/blocks/main-title.blade.php
@@ -1,8 +1,8 @@
 <div class="sub_header">
 	<div class="section_map_site">
-		<h4 class="text_map_site ktm_shadon_title ktm_white">Home / aviso de privacidad</h4>
+		<h4 class="text_map_site ktm_shadon_title ktm_white">Home / Seccion X / Subseccion</h4>
 	</div>
 	<div class="section_main_title">
-		<h2 class="text_main_title ktm_shadon_title ktm_white">Aviso de privacidad</h2>
+		<h2 class="text_main_title ktm_shadon_title ktm_white">Titulo de la seccion</h2>
 	</div>
 </div>
