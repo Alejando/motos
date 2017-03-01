336370156b73dedbe2e14c7c725d1d610f60da9a
gmanzanares <gmanzanares@192.168.2.95>
2017-03-01 10:11:34 -0600

    Recuperacion de rutas, migracion de tabla productType y foreing type_id

---
+++ fw/database/migrations/2017_03_01_153843_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_types', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('type_id')->unsigned();
+            $table->foreign('type_id')
+                ->references('id')
+                ->on('product_types')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_types');
+        
+        Schema::table('products', function(Blueprint $table){
+            $table->dropForeign('products_type_id_foreign');
+        });
+
+    }
+}
