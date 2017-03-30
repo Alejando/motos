a78097d7c9793d8e624cad85ba42215b6238a664
Francisco Diaz <jdiaz@estrasol.com.mx>
2017-03-30 17:30:28 -0600

    Entorno de desarrollo fco

--- fw/config/enviroments.php
+++ fw/config/enviroments.php
@@ -1,5 +1,6 @@
 <?php
 $env = [
+    '/var/www/html/jfcodiaz/dw.ktm-motos/fw'=> 'fco.env',
     '/var/www/html/jfcodiaz/dw.setpoint/fw' => 'demo-host-remoto.env',
 //    '/var/www/html/jfcodiaz/dw.setpoint/fw' => '.env',
     '/home/demoemx/public_html/setpoint/fw' => 'demo.env',
